6e8a40359c9aa4f64aaa921575fae28f53bb6f7e
roky-08 <roky@cheerful.com>
2017-04-04 03:55:42 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-2061735542105713",
+    enable_page_level_ads: true
+  });
+</script><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>
 -=SHAYAN_ALI=-
 
 </title><link rel="stylesheet" type="text/css" href="https://zindabad.herokuapp.com/awais.css" media="all,handheld"/><link rel="shortcut icon" href="">
